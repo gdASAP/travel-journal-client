d64f07675b6433ee0d51a5eb383e9a0c3ac550de
gdASAP <gerriedozier@gmail.com>
2020-10-31 17:29:35 -0400

    Removing extra / in routes

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://macabre-mansion-80824.herokuapp.com/',
+  production: 'https://macabre-mansion-80824.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
